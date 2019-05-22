ded6b5b5a121140f18bf001d6986a95b0d90e989
Bosslv0 <coreyjhunt@gmail.com>
2019-05-22 12:31:48 -0700

    all image assets for gameplay complete and loaded in boot

--- game_files/js/states/Boot.js
+++ game_files/js/states/Boot.js
@@ -1,5 +1,5 @@
 "use strict";
-// state to hadle load times
+// state to handle load times
 function Boot(game) {}
 	
 	Boot.prototype = {
@@ -8,11 +8,30 @@ function Boot(game) {}
             // preloading all assets before game starts
 			game.load.bitmapFont("myfont", "assets/imgs/pixel_fantasy.png", "assets/imgs/pixel_fantasy.fnt");
 			
-			game.load.image("player_side", "assets/imgs/player_side.png"); 
+			game.load.image("player_side", "assets/imgs/Blue-04.png"); 
 			game.load.image("weapon1","assets/imgs/projectile-blue.png");
-            game.load.image("enemy1", "assets/imgs/Red-02.png")
-            game.load.image("enemyWeapon", "assets/imgs/projectile-red.png")
-
+            game.load.image("enemy1", "assets/imgs/Dark-Grey-04.png");
+            game.load.image("enemy2", "assets/imgs/OctoMini.png");
+            game.load.image("enemy3", "assets/imgs/TankBarrel.png");
+            game.load.image("enemy3-1", "assets/imgs/TankBase.png");
+            game.load.image("enemy4", "assets/imgs/TriEnemy.png");
+            game.load.image("enemy5", "assets/imgs/TurretHead.png");
+            game.load.image("enemy5-1", "assets/imgs/TurretBase.png")
+            game.load.image("enemyWeapon", "assets/imgs/projectile-red.png");
+            game.load.image("pickup01", "assets/imgs/DoublePickup.png");
+            game.load.image("pickup02", "assets/imgs/ShotgunPickup.png");
+            game.load.image("pickup03", "assets/imgs/TriPickup.png");
+            game.load.image("pickup04", "assets/imgs/RailPickup.png");
+            game.load.image("pickup05", "assets/imgs/ShieldPickup.png");
+            game.load.image("debris1", "assets/imgs/debrisShip01.png");
+            game.load.image("debris2", "assets/imgs/debrisShip02.png");
+            game.load.image("debris3", "assets/imgs/debrisShip03.png");
+            game.load.image("debris4", "assets/imgs/debrisShip04.png");
+            game.load.image("debris5", "assets/imgs/debrisShip05.png");
+            game.load.image("ZZA", "assets/imgs/OctoBoss.png");
+            game.load.image("TentieTop", "assets/imgs/tentacle top.png");
+            game.load.image("TentieMid", "assets/imgs/Tentacle mid.png");
+            game.load.image("TentieBot", "assets/imgs/Tentacle bot.png");
             game.load.image("weapon2", "assets/imgs/ScatterShot.png");
             game.load.image("weapon3", "assets/imgs/SplitShot.png");
             game.load.image("weapon4", "assets/imgs/RailShot.png");
