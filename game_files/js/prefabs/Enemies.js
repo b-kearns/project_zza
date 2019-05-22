e1755bd6e08bbeffc5626a261ebe6fade5e06376
Bosslv0 <coreyjhunt@gmail.com>
2019-05-22 13:09:36 -0700

    some organization stuff

--- game_files/js/prefabs/Enemies.js
+++ game_files/js/prefabs/Enemies.js
@@ -6,7 +6,7 @@ function Enemy1(game, posX, posY, key) {
 	this.anchor.set(0.5);
     this.scale.setTo(-1, 1);
 	this.DRAG = 8000;
-	this.MAX_VELOCITY = 500;
+	this.MAX_VELOCITY = 200;
 	this.ACCELERATION = 1500;
 	this.HEALTH = 2;
 	
@@ -15,10 +15,10 @@ function Enemy1(game, posX, posY, key) {
 	this.outOfCameraBoundsKill = false;
 	this.autoCull = true;
 	this.body.drag.setTo(this.DRAG, this.DRAG);
-	this.body.maxVelocity.setTo(this.MAX_VELOCITY, this.MAX_VELOCITY);
+	this.body.maxVelocity.setTo(this.MAX_VELOCITY, 500);
     this.body.setSize(20, 20, 5, 10);
 	
-	this.weapon = new SingleShotE(game, this.position.x, this.position.y, -1, "enemyWeapon", 1);
+	this.weapon = new SingleShot(game, this.position.x, this.position.y, -1, "enemyWeapon", 1);
 }
 
 Enemy1.prototype = Object.create(Phaser.Sprite.prototype);
@@ -36,13 +36,6 @@ Enemy1.prototype.update = function() {
 	if(this.inCamera && !this.outOfCameraBoundsKill){
 		this.outOfCameraBoundsKill = true;
 	}
-	// sine wave movement
-	if(this.position.y < 310){
-	  this.body.acceleration.setTo(-200,400);
-    }
-    else if(this.position.y > 320 ){
-      this.body.acceleration.setTo(-200,-400); 
-    }
 	// allow player to kill with shots
 	if(this.HEALTH <= 0){
 		this.kill();
@@ -52,6 +45,55 @@ Enemy1.prototype.update = function() {
 }
 
 Enemy1.prototype.shoot = function(){
+	for(var i = 0; i < 20; i++){
+		this.weapon.fire(this);
+	}
+}
+function Enemy2(game, posX, posY, key) {
+	Phaser.Sprite.call(this,game, posX, posY, key);
+	// set enemy data
+	this.anchor.set(0.5);
+    this.scale.setTo(-1, 1);
+	this.DRAG = 8000;
+	this.MAX_VELOCITY = 500;
+	this.ACCELERATION = 1500;
+	this.HEALTH = 2;
+	
+	game.physics.enable(this);
+	this.body.collideWorldBounds = false;
+	this.outOfCameraBoundsKill = false;
+	this.autoCull = true;
+	this.body.drag.setTo(this.DRAG, this.DRAG);
+	this.body.maxVelocity.setTo(0, 300);
+    this.body.setSize(20, 20, 5, 10);
+	
+	this.weapon = new DoubleShot(game, this.position.x, this.position.y, -1, "enemyWeapon", 1);
+}
+
+Enemy2.prototype = Object.create(Phaser.Sprite.prototype);
+Enemy2.prototype.constructor = Enemy1;
+
+Enemy2.prototype.create = function() {
+	// var timer = game.time.create(false);
+	// timer.loop(200, this.shoot, this);
+	// timer.start();
+}
+
+Enemy2.prototype.update = function() {
+	//game.physics.arcade.overlap(this.weapon, GamePlay.player, GamePlay.collisionHandle, null, this);
+	//kill the object when is out of scope
+	if(this.inCamera && !this.outOfCameraBoundsKill){
+		this.outOfCameraBoundsKill = true;
+	}
+	// allow player to kill with shots
+	if(this.HEALTH <= 0){
+		this.kill();
+	}
+	// fire rate
+	if(this.alive && this.inCamera && game.rnd.integerInRange(1,100) > 90){this.weapon.fire(this);}
+}
+
+Enemy2.prototype.shoot = function(){
 	for(var i = 0; i < 20; i++){
 		this.weapon.fire(this);
 	}
--- game_files/js/prefabs/Weapons.js
+++ game_files/js/prefabs/Weapons.js
@@ -36,40 +36,6 @@ SingleShot.prototype.fire = function(source) {
 	
 }
 
-function SingleShotE(game, posX, posY, direction, key, ammo) {
-	Phaser.Group.call(this, game, game.world, "SingleShotE", false, true, Phaser.Physics.ARCADE);
-	
-	this.NAME = "Single";
-	this.DIRECTION = direction;
-	this.PENETRATE = false;
-	this.DAMAGE = 1;
-    this.nextFire = 0;
-	this.bulletSpeed = 600;
-	this.fireRate = 200;
-    this.SFX = game.add.audio("weapon_fx_1");
-	
-	for(var i = 0; i < ammo; i++){
-	this.add(new Bullet(game, "enemyWeapon"), true);
-	}
-}
-
-SingleShotE.prototype = Object.create(Phaser.Group.prototype);
-SingleShotE.prototype.constructor = SingleShotE;
-
-SingleShotE.prototype.fire = function(source) {
-	if(!source){return;}
-	if(game.time.time < this.nextFire){
-		return;
-	}
-	this.bullet = this.getFirstExists(false);
-	if(this.bullet === null){return;}
-    this.SFX.play();
-	this.getFirstExists(false).fire(this.DIRECTION, source.position.x, source.position.y, 0, this.bulletSpeed * this.DIRECTION, 0, 0);
-
-	this.nextFire = game.time.time + this.fireRate;
-	
-}
-
 function ScatterShot(game, posX, posY, direction, key, ammo) {
 	Phaser.Group.call(this, game, game.world, "ScatterShot", false, true, Phaser.Physics.ARCADE);
 	
