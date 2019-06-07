45be5fae436bb94995085333eb55031f7ad9154f
Brandon Kearns <bkearns@ucsc.edu>
2019-06-07 14:27:32 -0700

    ZZA beta

--- game_files/js/states/Zza.js
+++ game_files/js/states/Zza.js
@@ -22,23 +22,41 @@ Zza.prototype = {
 		// this.player = game.add.sprite(730, game.world.height, "Atlas", "blue_05");
 		this.player.flipPOV();
 		
-		this.Zza = game.add.sprite(200, -500, "Atlas", "OctoBoss");
+		this.Zza = game.add.sprite(game.world.centerX, -500, "Atlas", "OctoBoss");
+		this.Zza.anchor.setTo(0.5, 0.5);
 		
-		this.Zza1Top = game.add.sprite(this.Zza.position.x + 32, this.Zza.position.y + 190, "Atlas", "tentacle top");
-		this.Zza1Mid = game.add.sprite(this.Zza.position.x + 32, this.Zza.position.y + 240, "Atlas", "Tentacle mid");
-		this.Zza1Bot = game.add.sprite(this.Zza.position.x + 32, this.Zza.position.y + 300, "Atlas", "Tentacle bot");
+		this.Top1 = game.add.sprite(this.Zza.position.x - 64, this.Zza.position.y, "Atlas", "tentacle top");
+		this.Mid1 = game.add.sprite(this.Zza.position.x - 84, this.Zza.position.y, "Atlas", "Tentacle mid");
+		this.Bot1 = new Enemy6(game, this.Zza.position.x - 84, this.Zza.position.y, "Tentacle bot", 1);
+		game.add.existing(this.Bot1);
+		
+		this.Top2 = game.add.sprite(this.Zza.position.x - 40, this.Zza.position.y, "Atlas", "tentacle top");
+		this.Mid2 = game.add.sprite(this.Zza.position.x - 40, this.Zza.position.y, "Atlas", "Tentacle mid");
+		this.Bot2 = new Enemy6(game, this.Zza.position.x - 40, this.Zza.position.y, "Tentacle bot", 2);
+		game.add.existing(this.Bot2);
 
-		this.Zza2Top = game.add.sprite(this.Zza.position.x + 74, this.Zza.position.y + 225, "Atlas", "tentacle top");
-		this.Zza2Mid = game.add.sprite(this.Zza.position.x + 74, this.Zza.position.y + 275, "Atlas", "Tentacle mid");
-		this.Zza2Bot = game.add.sprite(this.Zza.position.x + 74, this.Zza.position.y + 335, "Atlas", "Tentacle bot");
+		this.Top3 = game.add.sprite(this.Zza.position.x + 40, this.Zza.position.y, "Atlas", "tentacle top");
+		this.Mid3 = game.add.sprite(this.Zza.position.x + 40, this.Zza.position.y, "Atlas", "Tentacle mid");
+		this.Bot3 = new Enemy6(game, this.Zza.position.x + 40, this.Zza.position.y, "Tentacle bot", 2);
+		game.add.existing(this.Bot3);
 
-		this.Zza3Top = game.add.sprite(this.Zza.position.x + 111, this.Zza.position.y + 225, "Atlas", "tentacle top");
-		this.Zza3Mid = game.add.sprite(this.Zza.position.x + 111, this.Zza.position.y + 275, "Atlas", "Tentacle mid");
-		this.Zza3Bot = game.add.sprite(this.Zza.position.x + 111, this.Zza.position.y + 335, "Atlas", "Tentacle bot");
-
-		this.Zza4Top = game.add.sprite(this.Zza.position.x + 148, this.Zza.position.y + 190, "Atlas", "tentacle top");
-		this.Zza4Mid = game.add.sprite(this.Zza.position.x + 148, this.Zza.position.y + 240, "Atlas", "Tentacle mid");
-		this.Zza4Bot = game.add.sprite(this.Zza.position.x + 148, this.Zza.position.y + 300, "Atlas", "Tentacle bot");
+		this.Top4 = game.add.sprite(this.Zza.position.x + 64, this.Zza.position.y, "Atlas", "tentacle top");
+		this.Mid4 = game.add.sprite(this.Zza.position.x + 84, this.Zza.position.y, "Atlas", "Tentacle mid");
+		this.Bot4 = new Enemy6(game, this.Zza.position.x + 84, this.Zza.position.y, "Tentacle bot", 1);
+		game.add.existing(this.Bot4);
+		
+		this.tent_1 = [this.Top1, this.Mid1, this.Bot1];
+		this.tent_2 = [this.Top2, this.Mid2, this.Bot2];
+		this.tent_3 = [this.Top3, this.Mid3, this.Bot3];
+		this.tent_4 = [this.Top4, this.Mid4, this.Bot4];
+		
+		this.tentacles = [this.tent_1, this.tent_2, this.tent_3, this.tent_4];
+		
+		for(var i = 0; i < this.tentacles.length; i++){
+			for(var j = 0; j < this.tentacles[i].length; j++){
+				this.tentacles[i][j].anchor.setTo(0.5, 0);
+			}
+		}
 		
 		this.plats = [];
 		this.plats[0]= game.add.tileSprite(-100,0,100,game.world.height,"Atlas", "leftPlat");
@@ -58,26 +76,58 @@ Zza.prototype = {
   
 		game.add.tween(this.plats[0]).to({x: -30}, 3000, "Linear", true, 0, 0, false);
 		game.add.tween(this.plats[1]).to({x: 570}, 3000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza).to({y: 0}, 5000, "Linear", true, 0, 0, false);//1
-		game.add.tween(this.Zza1Top).to({y: 190}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza1Mid).to({y: 240}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza1Bot).to({y: 300}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza2Top).to({y: 225}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza2Mid).to({y: 275}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza2Bot).to({y: 335}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza3Top).to({y: 225}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza3Mid).to({y: 275}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza3Bot).to({y: 335}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza4Top).to({y: 190}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza4Mid).to({y: 240}, 5000, "Linear", true, 0, 0, false);
-		game.add.tween(this.Zza4Bot).to({y: 300}, 5000, "Linear", true, 0, 0, false);
+		
+		game.add.tween(this.Zza).to({y: 164}, 5000, "Linear", true, 0, 0, false);
+		
+		game.add.tween(this.Top1).to({y: 230}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Mid1).to({y: 275}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Bot1).to({y: 330}, 5000, "Linear", true, 0, 0, false);
+		
+		game.add.tween(this.Top2).to({y: 270}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Mid2).to({y: 320}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Bot2).to({y: 375}, 5000, "Linear", true, 0, 0, false);
+		
+		game.add.tween(this.Top3).to({y: 270}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Mid3).to({y: 320}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Bot3).to({y: 375}, 5000, "Linear", true, 0, 0, false);
+		
+		game.add.tween(this.Top4).to({y: 230}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Mid4).to({y: 275}, 5000, "Linear", true, 0, 0, false);
+		game.add.tween(this.Bot4).to({y: 330}, 5000, "Linear", true, 0, 0, false);
+		
+		this.Top1.angle = 30;
+		this.Top4.angle = -30;
+		
+		this.Bot1.body.angularVelocity = 25;
+		this.Bot2.body.angularVelocity = 25;
+		this.Bot3.body.angularVelocity = -25;
+		this.Bot4.body.angularVelocity = -25;
+		
+		console.log(this.tentacles[0][2]);
 	},
 	update: function(){
 		this.plats[0].tilePosition.y -=3;
 		this.plats[1].tilePosition.y -=3;
 		this.background[0].tilePosition.y -=1;
+		
+		game.physics.arcade.overlap(this.tent_1[2], this.player.weapon, collisionHandle, null, this);
+		game.physics.arcade.overlap(this.tent_2[2], this.player.weapon, collisionHandle, null, this);
+		game.physics.arcade.overlap(this.tent_3[2], this.player.weapon, collisionHandle, null, this);
+		game.physics.arcade.overlap(this.tent_4[2], this.player.weapon, collisionHandle, null, this);
+		
+		checkCollision(this.tentacles[0][2]);
+		checkCollision(this.tentacles[1][2]);
+		checkCollision(this.tentacles[2][2]);
+		checkCollision(this.tentacles[3][2]);
+		
+		if(!this.tent_1[2].alive){this.tent_1[0].kill();this.tent_1[1].kill();}
+		if(!this.tent_2[2].alive){this.tent_2[0].kill();this.tent_2[1].kill();}
+		if(!this.tent_3[2].alive){this.tent_3[0].kill();this.tent_3[1].kill();}
+		if(!this.tent_4[2].alive){this.tent_4[0].kill();this.tent_4[1].kill();}
 	},
 	render: function(){
 		
+	},
+	tentacleCollision: function(tentacle, weapon){
 	}
 }
